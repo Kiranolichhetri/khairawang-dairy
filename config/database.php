30f0bd24aa651b23d3feb2be95c1ca95e2497840
Kiranolichhetri <kiranoli421@gmail.com>
2025-12-01 21:14:07 +0545

    commit

--- config/database.php
+++ config/database.php
@@ -2,29 +2,17 @@
 
 declare(strict_types=1);
 
-/**
- * Database Configuration
- * 
- * Database connection settings.
- */
-
 return [
-    /**
-     * Default database connection
-     */
     'default' => 'mysql',
 
-    /**
-     * Database connections
-     */
     'connections' => [
         'mysql' => [
             'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => 3306,
-            'database' => 'khairawang_dairy',
-            'username' => 'root',
-            'password' => '',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => (int) env('DB_PORT', 3306),
+            'database' => env('DB_DATABASE', 'khairawang_dairy'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -32,28 +20,8 @@ return [
                 \PDO::ATTR_PERSISTENT => false,
             ],
         ],
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => 'database/database.sqlite',
-            'prefix' => '',
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => '127.0.0. 1',
-            'port' => 5432,
-            'database' => 'khairawang_dairy',
-            'username' => 'postgres',
-            'password' => '',
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
     ],
 
-    /**
-     * Migration settings
-     */
     'migrations' => [
         'table' => 'migrations',
         'path' => 'database/migrations',
