f3abb63f8304af16f56f21fd909c511b44a6389d
Kiranolichhetri <kiranoli421@gmail.com>
2025-12-03 11:28:25 +0545

    commit

--- config/database.php
+++ config/database.php
@@ -3,33 +3,29 @@
 declare(strict_types=1);
 
 return [
-    // Hardcode to mongodb (env() is not loading properly)
-    'default' => 'mongodb',
+    "default" => "mongodb",
 
-    'connections' => [
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => (int) env('DB_PORT', 3306),
-            'database' => env('DB_DATABASE', 'khairawang_dairy'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'options' => [
-                \PDO::ATTR_PERSISTENT => false,
-            ],
+    "connections" => [
+        "mysql" => [
+            "driver" => "mysql",
+            "host" => "127.0.0.1",
+            "port" => 3306,
+            "database" => "khairawang_dairy",
+            "username" => "root",
+            "password" => "",
+            "charset" => "utf8mb4",
+            "collation" => "utf8mb4_unicode_ci",
+            "prefix" => "",
         ],
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'uri' => 'mongodb+srv://kiranoli421_db_user:keFt9XKE7oIdaAY2@cluster0.dxc9xkf.mongodb.net/',
-            'database' => 'khairawang_dairy',
+        "mongodb" => [
+            "driver" => "mongodb",
+            "uri" => "mongodb+srv://kiranoli421_db_user:keFt9XKE7oIdaAY2@cluster0.dxc9xkf.mongodb.net/",
+            "database" => "khairawang_dairy",
         ],
     ],
 
-    'migrations' => [
-        'table' => 'migrations',
-        'path' => 'database/migrations',
+    "migrations" => [
+        "table" => "migrations",
+        "path" => "database/migrations",
     ],
 ];
